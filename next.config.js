432d4b83d01e39b9f9e8e758d74ba4062509d4a2
Rafi <ruffyhacks@gmail.com>
2023-01-17 16:56:56 +0200

    removed commented-out code

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // async rewrites() {
-  //   return [
-  //     {
-  //       // source: '/:slug*',
-  //       source: '/:path*',
-  //       // destination: 'http://localhost:3001/api/:slug*'
-  //       destination: `${BACKEND_API_URL}/:path*`
-  //     },
-  //   ]
-  // },
 }
 
 module.exports = nextConfig
